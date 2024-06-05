9a6b7c06569fb8af1622aeb0572cfb1e039339ec
Walid Amriou <walidamriou@gmail.com>
2024-06-05 11:49:44 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <iostream> // Include the input/output stream library
+#include <cstdio>  // Include the C standard input and output library
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World! -- 1" << std::endl;
+    printf("Hello, World! -- 2\n");  // Print "Hello, World!" to the console
     return 0;
 }
